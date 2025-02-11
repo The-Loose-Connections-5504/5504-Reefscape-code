39381cd7117d9b92bcb98111a42f9f2caba6da25
The-Loose-Connections-5504 <team5504robotics@gmail.com>
2025-02-10 19:31:21 -0500

    Added a callback

--- src/main/java/frc/robot/subsystems/KennysArm.java
+++ src/main/java/frc/robot/subsystems/KennysArm.java
@@ -22,6 +22,7 @@ public class KennysArm extends SubsystemBase {
   public KennysArm() {
     //I blame Kenny if this doesn't work :P
     kRotatorConfig.idleMode(IdleMode.kBrake);
+    //I don't know what the hell this voltage  does but it's here and it might work sooo.......
     kKennyArmRotator.setVoltage(1);
     kKennyArmRotator.configure(kRotatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
